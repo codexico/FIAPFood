393775833423bb75eca10084f12b1c965c825d1c
codexico@gmail.com <codexico@gmail.com>
2016-02-13 16:50:35 -0200

    Get current location on map

--- app/src/main/java/com/example/fk/fiapfood/RestaurantAddActivity.java
+++ app/src/main/java/com/example/fk/fiapfood/RestaurantAddActivity.java
@@ -1,22 +1,238 @@
 package com.example.fk.fiapfood;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
-public class RestaurantAddActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class RestaurantAddActivity extends AppCompatActivity implements OnMapReadyCallback,
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+
+    protected static final String TAG = "FIAPFOOOOOOOOOOOOOOOOOD";
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+
+    private GoogleMap mMap;
+    protected Location currentLocation;
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_add);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapMini);
+        mapFragment.getMapAsync(this);
+
+        buildGoogleApiClient();
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.w(TAG, new Object() {}.getClass().getEnclosingMethod().getName());
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        createLocationRequest();
+    }
+
+    /**
+     * Sets up the location request. Android has two location request settings:
+     * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+     * the AndroidManifest.xml.
+     * <p/>
+     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+     * interval (5 seconds), the Fused Location Provider API returns location updates that are
+     * accurate to within a few feet.
+     * <p/>
+     * These settings are appropriate for mapping applications that show real-time location
+     * updates.
+     */
+    protected void createLocationRequest() {
+        Log.w(TAG, new Object() {
+        }.getClass().getEnclosingMethod().getName());
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    private void showLocation(Location location) {
+        Log.w(TAG, new Object() {}.getClass().getEnclosingMethod().getName());
+
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        LatLng here = new LatLng(lat, lng);
+
+//        if (mMapReady && mGoogleApiClient.isConnected()) {
+            mMap.addMarker(new MarkerOptions().position(here).title("I'm here!"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(here));
+//        }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.w(TAG, new Object() {
+        }.getClass().getEnclosingMethod().getName());
+        mMap = googleMap;
+
+        if (mGoogleApiClient.isConnected()) {
+            showLocation(currentLocation);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        startLocationUpdates();
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi.
+     */
+    protected void startLocationUpdates() {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+
+        // The final argument to {@code requestLocationUpdates()} is a LocationListener
+        // (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, new Object() {
+        }.getClass().getEnclosingMethod().getName());
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        showLocation(currentLocation);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onResume() {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        super.onResume();
+        // Within {@code onPause()}, we pause location updates, but leave the
+        // connection to GoogleApiClient intact.  Here, we resume receiving
+        // location updates if the user has requested them.
+
+//        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+//            startLocationUpdates();
+//        }
+    }
+
+    @Override
+    protected void onPause() {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        super.onPause();
+        // Stop location updates to save battery, but don't disconnect the GoogleApiClient object.
+        if (mGoogleApiClient.isConnected()) {
+//            stopLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        mGoogleApiClient.disconnect();
+
+        super.onStop();
     }
 
 }
