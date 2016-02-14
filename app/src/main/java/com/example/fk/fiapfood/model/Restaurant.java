6a6f45e3d27d24f717818b4cb9289253d9e7e7ed
codexico@gmail.com <codexico@gmail.com>
2016-02-14 18:13:39 -0200

    Add Restaurant Model

---
+++ app/src/main/java/com/example/fk/fiapfood/model/Restaurant.java
@@ -0,0 +1,87 @@
+package com.example.fk.fiapfood.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.Index;
+import io.realm.annotations.PrimaryKey;
+
+public class Restaurant extends RealmObject {
+
+    private String imageUrl;
+    @PrimaryKey
+    private String name;
+    private String phone;
+    @Index
+    private String type;
+    @Index
+    private int price;
+    private String observation;
+    @Index
+    private float latitude;
+    @Index
+    private float longitude;
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
