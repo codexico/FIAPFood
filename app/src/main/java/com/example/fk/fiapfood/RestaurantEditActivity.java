fb9bb1e1bc861fcd2fc4a75bdc1566850b0397f9
codexico@gmail.com <codexico@gmail.com>
2016-02-15 15:34:43 -0200

    Remove layoit elements

--- app/src/main/java/com/example/fk/fiapfood/RestaurantEditActivity.java
+++ app/src/main/java/com/example/fk/fiapfood/RestaurantEditActivity.java
@@ -87,14 +87,6 @@ public class RestaurantEditActivity extends AppCompatActivity implements OnMapRe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ButterKnife.bind(this);
